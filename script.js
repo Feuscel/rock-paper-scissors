4d67141a18d12844f04aba90ac3ae7966af0af48
Feuscel <feuscel@gmail.com>
2021-08-06 15:35:48 +0200

    game

---
+++ script.js
@@ -0,0 +1,33 @@
+const choice = ["Rock", "Paper", "Scissors"];
+
+function computerPlay (){
+    randomPlay = Math.floor(Math.random() * 3);
+    return choice[randomPlay];
+}
+
+function capitalize (str){
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function playRound(playerSelection, computerSelection){
+    let lowerPlayerSelection = playerSelection.toLowerCase();
+    let lowerComputerSelection = computerSelection.toLowerCase();
+    const equality = lowerComputerSelection === lowerPlayerSelection;
+    const playerWin = (lowerPlayerSelection === "paper"    && lowerComputerSelection === "rock") || 
+                      (lowerPlayerSelection === "scissors" && lowerComputerSelection === "paper") || 
+                      (lowerPlayerSelection === "rock"     && lowerComputerSelection === "scissors"); 
+    if (equality)
+        return "Equality";
+    if (playerWin)
+        return "You win ! " + capitalize(playerSelection) + " beats " + capitalize(computerSelection) ;
+    return "You lose ! " + capitalize(computerSelection) + " beats " + capitalize(playerSelection);
+}
+
+function game(){
+    for (let index = 0; index < 5; index++) {
+        console.log("Choose your play!");
+        console.log(playRound(prompt(), computerPlay()));
+    }
+}
+
+game();
